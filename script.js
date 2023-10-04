15a51384f5a5706b406bc439c015202ee358a565
Barbara Soares <barbarasoares1995@hotmail.com>
2023-10-04 12:18:53 -0300

    correção

--- script.js
+++ script.js
@@ -3,18 +3,45 @@ function gerarQRCode() {
     var qrCodeDiv = document.getElementById('qr-code');
     qrCodeDiv.innerHTML = '';
 
+    // Resolução mais alta (por exemplo, 300 pixels por polegada)
+    var dpi = 100;
+    var qrCodeSize = 2.5; // Tamanho em polegadas (exemplo: 2.5 x 2.5 polegadas)
+
     var qrcode = new QRCode(qrCodeDiv, {
         text: link,
-        width: 256,
-        height: 256
+        width: Math.round(dpi * qrCodeSize), // Largura em pixels
+        height: Math.round(dpi * qrCodeSize) // Altura em pixels
     });
 
-    // Adicionar margens à imagem do QR Code
-    var qrCodeImage = qrCodeDiv.getElementsByTagName('img')[0];
-    qrCodeImage.style.margin = '20px'; // Altere a margem conforme necessário
+    // Usar html2canvas para criar uma imagem da div do QR Code
+    html2canvas(qrCodeDiv, { dpi: dpi }).then(function(canvas) {
+        // Criar um canvas com a borda branca
+        var borderWidth = Math.round(0.1 * dpi); // 0.1 polegada de borda
+        var borderedCanvasWidth = canvas.width + 2 * borderWidth;
+        var borderedCanvasHeight = canvas.height + 2 * borderWidth;
 
-    // Exibir o botão de download após a geração do QR Code
-    var saveButton = document.getElementById('save-button');
-    saveButton.style.display = 'block';
-    saveButton.href = qrCodeImage.src;
+        var borderedCanvas = document.createElement('canvas');
+        borderedCanvas.width = borderedCanvasWidth;
+        borderedCanvas.height = borderedCanvasHeight;
+
+        var ctx = borderedCanvas.getContext('2d');
+
+        // Preencher o canvas com a borda branca
+        ctx.fillStyle = 'white';
+        ctx.fillRect(0, 0, borderedCanvasWidth, borderedCanvasHeight);
+
+        // Desenhar a imagem do QR Code no canvas com a borda branca
+        ctx.drawImage(canvas, borderWidth, borderWidth);
+
+        // Converter o canvas com a borda em uma imagem base64 em formato JPEG
+        var base64Image = borderedCanvas.toDataURL('image/jpeg', 0.9); // 0.9 é a qualidade, ajuste conforme necessário
+
+        // Exibir o botão de download após a geração do QR Code
+        var saveButton = document.getElementById('save-button');
+        saveButton.style.display = 'block';
+
+        // Definir o link de download
+        saveButton.href = base64Image;
+        saveButton.download = 'qrcode.jpg'; // Extensão .jpg para JPEG
+    });
 }
