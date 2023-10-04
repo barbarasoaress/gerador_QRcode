03a499c8e5e49a50b075fdc92c39931ae7346842
Barbara Soares <barbarasoares1995@hotmail.com>
2023-10-04 11:31:10 -0300

    correção bug

--- script.js
+++ script.js
@@ -2,16 +2,19 @@ function gerarQRCode() {
     var link = document.getElementById('link').value;
     var qrCodeDiv = document.getElementById('qr-code');
     qrCodeDiv.innerHTML = '';
+
     var qrcode = new QRCode(qrCodeDiv, {
         text: link,
         width: 256,
         height: 256
     });
-    
-    // Exiba o botão de download após a geração do QR Code
+
+    // Adicionar margens à imagem do QR Code
+    var qrCodeImage = qrCodeDiv.getElementsByTagName('img')[0];
+    qrCodeImage.style.margin = '20px'; // Altere a margem conforme necessário
+
+    // Exibir o botão de download após a geração do QR Code
     var saveButton = document.getElementById('save-button');
     saveButton.style.display = 'block';
-    
-    // Atualize o botão de download com o link da imagem do QR Code
-    saveButton.href = qrCodeDiv.getElementsByTagName('img')[0].src;
+    saveButton.href = qrCodeImage.src;
 }
